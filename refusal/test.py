9ee9b4327a863bfc5ec099d52ee222bf2e424081
manwaner <manwaner333@gmail.com>
2024-05-31 14:18:12 +0400

    31_05_2024

---
+++ refusal/test.py
@@ -0,0 +1,442 @@
+import json
+import torch
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from tqdm import tqdm
+import os
+from matplotlib import pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+from sklearn.manifold import TSNE
+# from IPython.display import display, HTML
+import matplotlib
+# token = input("Enter HF token: ")
+system_prompt = "You are a helpful, honest and concise assistant."
+data_path = "datasets/refusal/refusal_data_A_B_1.json"
+
+data = []
+with open(data_path, "r") as f:
+    data = json.load(f)
+
+def prompt_to_tokens(tokenizer, system_prompt, instruction, model_output):
+    B_INST, E_INST = "[INST]", "[/INST]"
+    B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+    dialog_content = B_SYS + system_prompt + E_SYS + instruction.strip()
+    dialog_tokens = tokenizer.encode(
+        f"{B_INST} {dialog_content.strip()} {E_INST} {model_output.strip()}"
+    )
+    return torch.tensor(dialog_tokens).unsqueeze(0)
+
+class ComparisonDataset(Dataset):
+    def __init__(self, data, system_prompt):
+        self.data = data
+        self.system_prompt = system_prompt
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "huggyllama/llama-7b"      # , use_auth_token=token
+        )
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        item = self.data[idx]
+        question = item["question"]
+        pos_answer = item["answer_matching_behavior"]
+        neg_answer = item["answer_not_matching_behavior"]
+        pos_tokens = prompt_to_tokens(
+            self.tokenizer, self.system_prompt, question, pos_answer
+        )
+        neg_tokens = prompt_to_tokens(
+            self.tokenizer, self.system_prompt, question, neg_answer
+        )
+        return pos_tokens, neg_tokens
+
+
+def add_vector_after_position(matrix, vector, position_ids, after=None):
+    after_id = after
+    if after_id is None:
+        after_id = position_ids.min().item() - 1
+    mask = position_ids > after_id
+    mask = mask.unsqueeze(-1)
+    matrix += mask.float() * vector
+    return matrix
+
+
+def find_subtensor_position(tensor, sub_tensor):
+    n, m = tensor.size(0), sub_tensor.size(0)
+    if m > n:
+        return -1
+    for i in range(n - m + 1):
+        if torch.equal(tensor[i : i + m], sub_tensor):
+            return i
+    return -1
+
+
+def find_instruction_end_postion(tokens, end_str):
+    end_pos = find_subtensor_position(tokens, end_str)
+    return end_pos + len(end_str) - 1
+
+
+class AttnWrapper(torch.nn.Module):
+    def __init__(self, attn):
+        super().__init__()
+        self.attn = attn
+        self.activations = None
+
+    def forward(self, *args, **kwargs):
+        output = self.attn(*args, **kwargs)
+        self.activations = output[0]
+        return output
+
+
+class BlockOutputWrapper(torch.nn.Module):
+    def __init__(self, block, unembed_matrix, norm, tokenizer):
+        super().__init__()
+        self.block = block
+        self.unembed_matrix = unembed_matrix
+        self.norm = norm
+        self.tokenizer = tokenizer
+
+        self.block.self_attn = AttnWrapper(self.block.self_attn)
+        self.post_attention_layernorm = self.block.post_attention_layernorm
+
+        self.attn_out_unembedded = None
+        self.intermediate_resid_unembedded = None
+        self.mlp_out_unembedded = None
+        self.block_out_unembedded = None
+
+        self.activations = None
+        self.add_activations = None
+        self.after_position = None
+
+        self.save_internal_decodings = False
+
+        self.calc_dot_product_with = None
+        self.dot_products = []
+
+    def forward(self, *args, **kwargs):
+        output = self.block(*args, **kwargs)
+        self.activations = output[0]
+        if self.calc_dot_product_with is not None:
+            last_token_activations = self.activations[0, -1, :]
+            decoded_activations = self.unembed_matrix(self.norm(last_token_activations))
+            top_token_id = torch.topk(decoded_activations, 1)[1][0]
+            top_token = self.tokenizer.decode(top_token_id)
+            dot_product = torch.dot(last_token_activations, self.calc_dot_product_with)
+            self.dot_products.append((top_token, dot_product.cpu().item()))
+        if self.add_activations is not None:
+            augmented_output = add_vector_after_position(
+                matrix=output[0],
+                vector=self.add_activations,
+                position_ids=kwargs["position_ids"],
+                after=self.after_position,
+            )
+            output = (augmented_output + self.add_activations,) + output[1:]
+
+        if not self.save_internal_decodings:
+            return output
+
+        # Whole block unembedded
+        self.block_output_unembedded = self.unembed_matrix(self.norm(output[0]))
+
+        # Self-attention unembedded
+        attn_output = self.block.self_attn.activations
+        self.attn_out_unembedded = self.unembed_matrix(self.norm(attn_output))
+
+        # Intermediate residual unembedded
+        attn_output += args[0]
+        self.intermediate_resid_unembedded = self.unembed_matrix(self.norm(attn_output))
+
+        # MLP unembedded
+        mlp_output = self.block.mlp(self.post_attention_layernorm(attn_output))
+        self.mlp_out_unembedded = self.unembed_matrix(self.norm(mlp_output))
+
+        return output
+
+    def add(self, activations):
+        self.add_activations = activations
+
+    def reset(self):
+        self.add_activations = None
+        self.activations = None
+        self.block.self_attn.activations = None
+        self.after_position = None
+        self.calc_dot_product_with = None
+        self.dot_products = []
+
+
+class Llama7BChatHelper:
+    def __init__(self, system_prompt):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.system_prompt = system_prompt
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            "huggyllama/llama-7b"    # , use_auth_token=token
+        )
+        self.model = AutoModelForCausalLM.from_pretrained(
+            "huggyllama/llama-7b"    # , use_auth_token=token
+        ).to(self.device)
+        self.END_STR = torch.tensor(self.tokenizer.encode("[/INST]")[1:]).to(
+            self.device
+        )
+        for i, layer in enumerate(self.model.model.layers):
+            self.model.model.layers[i] = BlockOutputWrapper(
+                layer, self.model.lm_head, self.model.model.norm, self.tokenizer
+            )
+
+    def set_save_internal_decodings(self, value):
+        for layer in self.model.model.layers:
+            layer.save_internal_decodings = value
+
+    def set_after_positions(self, pos):
+        for layer in self.model.model.layers:
+            layer.after_position = pos
+
+    def prompt_to_tokens(self, instruction):
+        B_INST, E_INST = "[INST]", "[/INST]"
+        B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+        dialog_content = B_SYS + self.system_prompt + E_SYS + instruction.strip()
+        dialog_tokens = self.tokenizer.encode(
+            f"{B_INST} {dialog_content.strip()} {E_INST}"
+        )
+        return torch.tensor(dialog_tokens).unsqueeze(0)
+
+    def generate_text(self, prompt, max_new_tokens=50):
+        tokens = self.prompt_to_tokens(prompt).to(self.device)
+        return self.generate(tokens, max_new_tokens=max_new_tokens)
+
+    def generate(self, tokens, max_new_tokens=50):
+        instr_pos = find_instruction_end_postion(tokens[0], self.END_STR)
+        self.set_after_positions(instr_pos)
+        generated = self.model.generate(
+            inputs=tokens, max_new_tokens=max_new_tokens, top_k=1
+        )
+        return self.tokenizer.batch_decode(generated)[0]
+
+    def get_logits(self, tokens):
+        with torch.no_grad():
+            logits = self.model(tokens).logits
+            return logits
+
+    def get_last_activations(self, layer):
+        return self.model.model.layers[layer].activations
+
+    def set_add_activations(self, layer, activations):
+        self.model.model.layers[layer].add(activations)
+
+    def set_calc_dot_product_with(self, layer, vector):
+        self.model.model.layers[layer].calc_dot_product_with = vector
+
+    def get_dot_products(self, layer):
+        return self.model.model.layers[layer].dot_products
+
+    def reset_all(self):
+        for layer in self.model.model.layers:
+            layer.reset()
+
+    def print_decoded_activations(self, decoded_activations, label, topk=10):
+        data = self.get_activation_data(decoded_activations, topk)[0]
+        print(label, data)
+
+    def decode_all_layers(
+        self,
+        tokens,
+        topk=10,
+        print_attn_mech=True,
+        print_intermediate_res=True,
+        print_mlp=True,
+        print_block=True,
+    ):
+        tokens = tokens.to(self.device)
+        self.get_logits(tokens)
+        for i, layer in enumerate(self.model.model.layers):
+            print(f"Layer {i}: Decoded intermediate outputs")
+            if print_attn_mech:
+                self.print_decoded_activations(
+                    layer.attn_out_unembedded, "Attention mechanism", topk=topk
+                )
+            if print_intermediate_res:
+                self.print_decoded_activations(
+                    layer.intermediate_resid_unembedded,
+                    "Intermediate residual stream",
+                    topk=topk,
+                )
+            if print_mlp:
+                self.print_decoded_activations(
+                    layer.mlp_out_unembedded, "MLP output", topk=topk
+                )
+            if print_block:
+                self.print_decoded_activations(
+                    layer.block_output_unembedded, "Block output", topk=topk
+                )
+
+    def plot_decoded_activations_for_layer(self, layer_number, tokens, topk=10):
+        tokens = tokens.to(self.device)
+        self.get_logits(tokens)
+        layer = self.model.model.layers[layer_number]
+
+        data = {}
+        data["Attention mechanism"] = self.get_activation_data(
+            layer.attn_out_unembedded, topk
+        )[1]
+        data["Intermediate residual stream"] = self.get_activation_data(
+            layer.intermediate_resid_unembedded, topk
+        )[1]
+        data["MLP output"] = self.get_activation_data(layer.mlp_out_unembedded, topk)[1]
+        data["Block output"] = self.get_activation_data(
+            layer.block_output_unembedded, topk
+        )[1]
+
+        # Plotting
+        fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(8, 6))
+        fig.suptitle(f"Layer {layer_number}: Decoded Intermediate Outputs", fontsize=21)
+
+        for ax, (mechanism, values) in zip(axes.flatten(), data.items()):
+            tokens, scores = zip(*values)
+            ax.barh(tokens, scores, color="skyblue")
+            ax.set_title(mechanism)
+            ax.set_xlabel("Value")
+            ax.set_ylabel("Token")
+
+            # Set scientific notation for x-axis labels when numbers are small
+            ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+            ax.ticklabel_format(style="sci", scilimits=(0, 0), axis="x")
+
+        plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+        plt.show()
+
+    def get_activation_data(self, decoded_activations, topk=10):
+        softmaxed = torch.nn.functional.softmax(decoded_activations[0][-1], dim=-1)
+        values, indices = torch.topk(softmaxed, topk)
+        probs_percent = [int(v * 100) for v in values.tolist()]
+        tokens = self.tokenizer.batch_decode(indices.unsqueeze(-1))
+        return list(zip(tokens, probs_percent)), list(zip(tokens, values.tolist()))
+
+
+
+def save_activation_projection_tsne(
+    activations1,
+    activations2,
+    fname,
+    title,
+    label1="Positive Examples",
+    label2="Negative Examples",
+):
+    """
+    activations1: n_samples x vector dim tensor
+    activations2: n_samples x vector dim tensor
+
+    projects to n_samples x 2 dim tensor using t-SNE (over the full dataset of both activations 1 and 2) and saves visualization.
+    Colors projected activations1 as blue and projected activations2 as red.
+    """
+    plt.clf()
+    activations = torch.cat([activations1, activations2], dim=0)
+    activations_np = activations.cpu().numpy()
+
+    # t-SNE transformation
+    tsne = TSNE(n_components=2)
+    projected_activations = tsne.fit_transform(activations_np)
+
+    # Splitting back into activations1 and activations2
+    activations1_projected = projected_activations[: activations1.shape[0]]
+    activations2_projected = projected_activations[activations1.shape[0] :]
+
+    # Visualization
+    for x, y in activations1_projected:
+        plt.scatter(x, y, color="blue", marker="o", alpha=0.4)
+
+    for x, y in activations2_projected:
+        plt.scatter(x, y, color="red", marker="o", alpha=0.4)
+
+    # Adding the legend
+    scatter1 = plt.Line2D(
+        [0],
+        [0],
+        marker="o",
+        color="w",
+        markerfacecolor="blue",
+        markersize=10,
+        label=label1,
+    )
+    scatter2 = plt.Line2D(
+        [0],
+        [0],
+        marker="o",
+        color="w",
+        markerfacecolor="red",
+        markersize=10,
+        label=label2,
+    )
+
+    plt.legend(handles=[scatter1, scatter2])
+    plt.title(title)
+    plt.xlabel("t-SNE 1")
+    plt.ylabel("t-SNE 2")
+    plt.savefig(fname)
+
+
+def plot_all_activations(layers):
+    if not os.path.exists("clustering"):
+        os.mkdir("clustering")
+    for layer in layers:
+        pos = torch.load(f"positive_layer_{layer}.pt")
+        neg = torch.load(f"negative_layer_{layer}.pt")
+        save_activation_projection_tsne(
+            pos,
+            neg,
+            f"clustering/activations_layer_{layer}.png",
+            f"t-SNE projected activations layer {layer}",
+        )
+
+
+
+def generate_and_save_steering_vectors(
+    model, dataset, start_layer=0, end_layer=32, token_idx=-2
+):
+    layers = list(range(start_layer, end_layer + 1))
+    positive_activations = dict([(layer, []) for layer in layers])
+    negative_activations = dict([(layer, []) for layer in layers])
+    model.set_save_internal_decodings(False)
+    model.reset_all()
+    for p_tokens, n_tokens in tqdm(dataset, desc="Processing prompts"):
+        p_tokens = p_tokens.to(model.device)
+        n_tokens = n_tokens.to(model.device)
+        model.reset_all()
+        model.get_logits(p_tokens)
+        for layer in layers:
+            p_activations = model.get_last_activations(layer)
+            p_activations = p_activations[0, token_idx, :].detach().cpu()
+            positive_activations[layer].append(p_activations)
+        model.reset_all()
+        model.get_logits(n_tokens)
+        for layer in layers:
+            n_activations = model.get_last_activations(layer)
+            n_activations = n_activations[0, token_idx, :].detach().cpu()
+            negative_activations[layer].append(n_activations)
+    for layer in layers:
+        positive = torch.stack(positive_activations[layer])
+        negative = torch.stack(negative_activations[layer])
+        vec = (positive - negative).mean(dim=0)
+        torch.save(vec, f"vec_layer_{layer}.pt")
+        torch.save(positive, f"positive_layer_{layer}.pt")
+        torch.save(
+            negative,
+            f"negative_layer_{layer}.pt",
+        )
+
+
+
+dataset = ComparisonDataset(data, system_prompt)
+print(f"Using {len(dataset)} samples")
+
+model = Llama7BChatHelper(system_prompt)
+
+start_layer = 0
+end_layer = 31
+generate_and_save_steering_vectors(model, dataset, start_layer=start_layer, end_layer=end_layer)
+plot_all_activations(list(range(start_layer, end_layer + 1)))
+
+
+
+
+
+qingli = 3
